101d6b4b0cc5569ef2e80df6345cb3a165432fa2
Zihao Wang <zwang688@gmail.com>
2022-07-18 20:24:11 -0400

    Add Abstract Factory exersice Ship

---
+++ Design Pattern/Abstract Factory/Examples/Ship/src/ShipFactory/EnemyShipFactory.java
@@ -0,0 +1,18 @@
+package ShipFactory;
+
+import Engine.ESEngine;
+import Weapon1.ESWeapon;
+
+//With an Abstract Factory Pattern you won't
+//just build ships, but also all of the components
+//for the ships
+
+//Here is where you define the parts that are required
+//if an object wants to be an enemy ship
+
+public interface EnemyShipFactory{
+	
+	public ESWeapon addESGun();
+	public ESEngine addESEngine();
+	
+}
